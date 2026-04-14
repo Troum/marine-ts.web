e6356d5b00f1620223860dd3474fde7f8bc97220
Ruslan Belchenkov <r.belchenkov@litefinance.com>
2026-04-14 17:36:22 +0300

    Update with architecture

--- app/utils/navigationDefaults.ts
+++ app/utils/navigationDefaults.ts
@@ -6,6 +6,7 @@ export const navigationMenuDefaults: NavigationMenuSettings = {
     { path: '/', label: { ru: 'Главная', en: 'Home' } },
     { path: '/about', label: { ru: 'О компании', en: 'About' } },
     { path: '/services', label: { ru: 'Услуги', en: 'Services' } },
+    { path: '/vacancies', label: { ru: 'Вакансии', en: 'Careers' } },
     { path: '/contacts', label: { ru: 'Контакты', en: 'Contacts' } },
   ],
   more: [
